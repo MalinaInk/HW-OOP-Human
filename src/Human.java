da83f75ca0f6102c3c7ddc6916064c6d3dc4d339
mo <nataleshen.ko@yandex.ru>
2022-10-25 12:31:33 +0300

    final branch commit - 4

--- src/Human.java
+++ src/Human.java
@@ -5,14 +5,32 @@ public class Human {
    String job;
 
    Human(String name, String town, String job, int yearOfBirth) {
-      this.name = name;
-      this.town = town;
-      this.job = job;
-      this.yearOfBirth = yearOfBirth;
+      if (name == null || name.isEmpty()) {
+         this.name = " Информация не указана ";
+      } else {  this.name = name;
+      }
+
+      if (town == null || town.isEmpty()) {
+         this.town = " Информация не указана ";
+      } else { this.town = town;
+      }
+
+      if (job == null || job.isEmpty()) {
+         this.job = " Информация не указана ";
+      } else {         this.job = job;
+      }
+
+      if (2022 - yearOfBirth > 0) {
+         this.yearOfBirth = 2022 - yearOfBirth;
+      } else {
+         this.yearOfBirth = 0;
+      }
    }
-   void say() {
-      System.out.println("Привет! Меня зовут " + name +
-              ". Я из города " + town + ". Я родился в " + (2022-yearOfBirth) +
-              " году. Я работаю на должности " + job + ". Будем знакомы!");
+
+       void say() {
+         System.out.println("Привет! Меня зовут " + name +
+                 ". Я из города " + town + ". Я родился в " + yearOfBirth +
+                 " году. Я работаю на должности " + job + ". Будем знакомы!");
+      }
    }
-}
+
--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        Human maxim = new Human("Максим","Минск","бренд-менеджер", 35);
+        Human maxim = new Human("Максим","Минск","бренд-менеджер", 2035);
         maxim.say();
 
-        Human ania = new Human("Аня", "Москва", "методист образовательных программ", 29);
+        Human ania = new Human("Аня", null, "методист образовательных программ", 29);
         ania.say();
 
-        Human katia = new Human("Катя","Калининград","продакт-менеджер", 28);
+        Human katia = new Human("Катя","Калининград","", 28);
         katia.say();
 
         Human artiom = new Human("Артем","Москва","директор по развитию бизнеса", 27);
